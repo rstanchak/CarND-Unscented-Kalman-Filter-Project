c8bdcc20a7d821de1f69b7dc6be82422f6863084
Roman Stanchak <rstanchak@semaconnect.com>
2017-07-12 08:08:59 -0400

    tools: implement RMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -10,9 +10,15 @@ Tools::Tools() {}
 Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+        const vector<VectorXd> &ground_truth) {
+  VectorXd rmse(4, 1);
+  rmse << 0, 0, 0, 0;
+  for(size_t i = 0; i<estimations.size(); ++i)
+  {   
+      VectorXd c = estimations[i]-ground_truth[i];
+      rmse = rmse.array() + c.array()*c.array();
+  }
+  rmse = rmse.array()/estimations.size();
+  rmse = rmse.array().sqrt();
+  return rmse;
 }
